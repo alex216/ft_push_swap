84062c42e238bd2b125e4a3e9896c1d778f5c8d5
yliu <yliu@XLIJFELKNIX.local>
2024-01-18 14:13:38 +0900

    [feat] node three ok

--- src/utils/ope_three_node.c
+++ src/utils/ope_three_node.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:39:43 by yliu              #+#    #+#             */
-/*   Updated: 2024/01/14 21:08:34 by yliu             ###   ########.fr       */
+/*   Updated: 2024/01/18 12:19:28 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ static bool	_is_descending_order(int first, int second, int third)
 	return (first > second && second > third);
 }
 
-void	ope_three_node(t_lst **stack_a, t_lst **lst_procedure)
+void	ope_three_node(t_lst **stack_a, t_lst **stack_b, t_lst **lst_procedure)
 {
 	(void)lst_procedure;
 
@@ -44,14 +44,14 @@ void	ope_three_node(t_lst **stack_a, t_lst **lst_procedure)
 	second = get_int_value_of((*stack_a)->next_p);
 	third = get_int_value_of((*stack_a)->next_p->next_p);
 	if (_is_two_one_three(first, second, third))
-		return (operate_sa(stack_a, lst_procedure));
+		return (operate_sa(stack_a, stack_b, lst_procedure));
 	else if (_is_three_one_two(first, second, third))
-		return (operate_ra(stack_a, lst_procedure));
+		return (operate_ra(stack_a, stack_b, lst_procedure));
 	else if (_is_two_three_one(first, second, third))
-		return (operate_rra(stack_a, lst_procedure));
-	operate_sa(stack_a, lst_procedure);
+		return (operate_rra(stack_a, stack_b, lst_procedure));
+	operate_sa(stack_a, stack_b, lst_procedure);
 	if (_is_descending_order(first, second, third))
-		return (operate_rra(stack_a, lst_procedure));
+		return (operate_rra(stack_a, stack_b, lst_procedure));
 	else
-		return (operate_ra(stack_a, lst_procedure));
+		return (operate_ra(stack_a, stack_b, lst_procedure));
 }
