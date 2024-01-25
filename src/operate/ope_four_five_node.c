0b09ea9d25fb6be4c9a20b2f0e6db6bfcbe54496
yliu <yliu@XLIJFELKNIX.local>
2024-01-25 14:07:15 +0900

    [feat] add func sort in optimized way

--- src/operate/ope_four_five_node.c
+++ src/operate/ope_four_five_node.c
@@ -6,7 +6,7 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:19:28 by yliu              #+#    #+#             */
-/*   Updated: 2024/01/25 14:01:39 by yliu             ###   ########.fr       */
+/*   Updated: 2024/01/25 14:04:02 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,34 @@ static size_t _return_number_to_rotate(t_lst **lst_pp, const t_lst *node_p)
 	return (ra_counter);
 }
 
+static	bool	_rotate_stack_in_shortest_way(t_lst **lst_pp, t_lst ** lst_procedure, size_t ra_counter)
+{
+	size_t	lst_size;
+
+	lst_size = ft_dl_lstsize(*lst_pp);
+	if (2 * ra_counter < lst_size)
+		while (ra_counter--)
+			operate_ra(lst_pp, lst_pp, lst_procedure);
+	else
+		while (lst_size - ra_counter++)
+			operate_rra(lst_pp, lst_pp, lst_procedure);
+	return (true);
+}
+
+// static void	_sort_stack_using_only_ra_or_rra(t_lst **lst_pp, t_lst **lst_procedure)
+// {
+// 	size_t	top_idx;
+// 	size_t	lst_size;
+//
+// 	top_idx = get_index_of(*lst_pp);
+// 	lst_size = ft_dl_lstsize(*lst_pp);
+// 	if (2 * top_idx < lst_size)
+// 		while (top_idx--)
+// 			operate_ra(lst_pp, lst_pp, lst_procedure);
+// 	else
+// 		while (lst_size - top_idx++)
+// 			operate_rra(lst_pp, lst_pp, lst_procedure);
+// }
 
 void	ope_four_five_node(t_lst **stack_a, t_lst **stack_b, t_lst **lst_procedure)
 {
@@ -50,7 +78,7 @@ void	ope_four_five_node(t_lst **stack_a, t_lst **stack_b, t_lst **lst_procedure)
 	{
 		ra_counter = _return_number_to_rotate(stack_a, *stack_b);
 		ft_printf("ra counter %d\n", ra_counter);
-		// _rotate_stack_in_shortest_way(stack_a, lst_procedure, ra_counter);
+		_rotate_stack_in_shortest_way(stack_a, lst_procedure, ra_counter);
 		operate_pa(stack_a, stack_b, lst_procedure);
 		debug_func(stack_a, stack_b, lst_procedure, "a\t", "b\t", "lst\t");
 	}
