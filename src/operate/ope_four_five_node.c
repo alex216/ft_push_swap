615f21b91f20b9eb5a78fe5ecae1f08b3793be68
yliu <yliu@XLIJFELKNIX.local>
2024-01-25 14:03:40 +0900

    [feat] add return ra_counter

--- src/operate/ope_four_five_node.c
+++ src/operate/ope_four_five_node.c
@@ -6,49 +6,55 @@
 /*   By: yliu <yliu@student.42.jp>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/13 16:19:28 by yliu              #+#    #+#             */
-/*   Updated: 2024/01/24 18:06:14 by yliu             ###   ########.fr       */
+/*   Updated: 2024/01/25 14:01:39 by yliu             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "push_swap.h"
 
-static bool	_is_time_to_pa(t_lst **stack_a, t_lst **stack_b)
+static size_t _return_number_to_rotate(t_lst **lst_pp, const t_lst *node_p)
 {
-	int	a_top;
-	int	a_tail;
+	int a_curr;
+	int	a_prev;
 	int	b_top;
+	size_t	ra_counter;
+	t_lst	*iter_p;
 
-	a_top = get_int_value_of(*stack_a);
-	a_tail = get_int_value_of(ft_dl_lstlast(*stack_a));
-	b_top = get_int_value_of(*stack_b);
-	if (a_top < a_tail)
-		return (b_top < a_top || b_top > a_tail);
-	else
-		return (a_tail < b_top && b_top < a_top);
+	if (!*lst_pp || ft_dl_lstsize(*lst_pp) == 1)
+		return (0);
+	ra_counter = 1;
+	iter_p = (*lst_pp)->next_p;
+	b_top = get_index_of(node_p);
+	while (iter_p->is_sentinel == false)
+	{
+		a_curr = get_index_of(iter_p);
+		a_prev = get_index_of(iter_p->prev_p);
+		if (a_prev < b_top && b_top < a_curr)
+			break;
+		iter_p = iter_p->next_p;
+		ra_counter++;
+	}
+	return (ra_counter);
 }
 
-static bool _put_node_to_right_pos(t_lst **stack_a, t_lst **stack_b, t_lst **lst_procedure)
-{
-	if (_is_time_to_pa(stack_a, stack_b))
-		return (operate_pa(stack_a, stack_b, lst_procedure));
-	operate_ra(stack_a, stack_b, lst_procedure);
-	// operate_rra(stack_a, stack_b, lst_procedure);
-	_put_node_to_right_pos(stack_a, stack_b, lst_procedure);
-	return (true);
-}
 
 void	ope_four_five_node(t_lst **stack_a, t_lst **stack_b, t_lst **lst_procedure)
 {
+	size_t ra_counter;
+
 	while (ft_dl_lstsize(*stack_a) > 3)
 		operate_pb(stack_a, stack_b, lst_procedure);
 	ope_three_node(stack_a, stack_b, lst_procedure);
-	// debug_func(stack_a, stack_b, lst_procedure, "a", "b" ,"lst");
 
 	while (*stack_b)
-		_put_node_to_right_pos(stack_a, stack_b, lst_procedure);
-	// debug_func(stack_a, stack_b, lst_procedure, "a", "b" ,"lst");
-
-	while(!is_ascending_order(*stack_a))
-		operate_ra(stack_a, stack_b, lst_procedure);
-	// debug_func(stack_a, stack_b, lst_procedure, "a", "b" ,"lst");
+	{
+		ra_counter = _return_number_to_rotate(stack_a, *stack_b);
+		ft_printf("ra counter %d\n", ra_counter);
+		// _rotate_stack_in_shortest_way(stack_a, lst_procedure, ra_counter);
+		operate_pa(stack_a, stack_b, lst_procedure);
+		debug_func(stack_a, stack_b, lst_procedure, "a\t", "b\t", "lst\t");
+	}
+	// debug_func(stack_a, stack_b, lst_procedure, "a\t", "b\t", "lst\t");
+	// _sort_stack_using_only_ra_or_rra(stack_a, lst_procedure);
+	// debug_func(stack_a, stack_b, lst_procedure, "a\t", "b\t", "lst\t");
 }
